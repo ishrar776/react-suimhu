576640e61ee293cf58f159fe6b6231870c28a5d2
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-08-26 15:00:38 +0530

    1 file modified

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../counter/CounterSlice';
-
+import counterSlice from '../counter/CounterSlice';
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    counter: counterSlice,
   },
 });
