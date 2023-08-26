17dcc39673ceb76c2eb25f3febfc2883421850a7
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-08-26 16:17:29 +0530

    1 file modified

--- src/counter/DemoCounter.js
+++ src/counter/DemoCounter.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {enterName} from './CounterSlice';
+import { enterName } from './CounterSlice';
 const DemoCounter = () => {
   const count = useSelector((item) => item.counter1.candidateName);
   const dispatch = useDispatch();
-  return <div>Hellow {count}</div>;
+  return (
+    <>
+      <div>Hellow {count}</div>
+      <div>
+        <button onClick={() => dispatch(enterName())}>+</button>
+      </div>
+    </>
+  );
 };
 export default DemoCounter;
