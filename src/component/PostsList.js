4f123c908e9dd18fd8845e6918bc6b6e0c2e04aa
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-08-29 15:19:03 +0530

    2 files modified

--- src/component/PostsList.js
+++ src/component/PostsList.js
@@ -1,30 +1,33 @@
-import React, { useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { selectAllPosts } from '../reducer/postSlice';
 import PostAuthor from '../component/PostAuthor';
 import TimeAgo from '../component/TimeAgo';
 import ReactionButtons from '../component/ReactionButtons';
 const PostsList = () => {
-  const posts = useSelector(selectAllPosts)
-  console.log("values from right" + posts)
-  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+  const posts = useSelector(selectAllPosts);
+  console.log('values from right' + posts);
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
 
-  const renderedPosts = orderedPosts.map(post => (
-      <article key={post.id}>
-          <h3>{post.title}</h3>
-          <p>{post.content.substring(0, 100)}</p>
-          <p className="postCredit">
-              <PostAuthor userId={post.userId} />
-              <TimeAgo timestamp={post.date} />
-          </p>
-          <ReactionButtons post={post} />
-      </article>
-  ))
+  const renderedPosts = orderedPosts.map((post) => (
+    <article key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content.substring(0, 100)}</p>
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+      </p>
+      <ReactionButtons post={post} />
+    </article>
+  ));
 
   return (
-      <section>
-          <h2>Posts</h2>
-          {renderedPosts}
-      </section>
-  )
-}
-export default PostsList
+    <section>
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  );
+};
+export default PostsList;
