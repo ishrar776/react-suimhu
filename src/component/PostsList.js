2a5687907cd0de29f702a120d98243dbe766a36c
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-08-29 15:00:10 +0530

    1 file modified

--- src/component/PostsList.js
+++ src/component/PostsList.js
@@ -4,26 +4,27 @@ import PostAuthor from '../component/PostAuthor';
 import TimeAgo from '../component/TimeAgo';
 import ReactionButtons from '../component/ReactionButtons';
 const PostsList = () => {
-  const posts = useSelector(selectAllPosts);
-  const orderedPosts = posts
-    .slice()
-    .sort((a, b) => b.date.localeCompare(a.date));
-  const renderedPosts = posts.map((post) => (
-    <article key={post.id}>
-      <h3>{post.title}</h3>
-      <p>{post.content.substring(0, 100)}</p>
-      <p className="postCredit">
-        <PostAuthor userId={post.userId} />
-        <TimeAgo timestamp={post.date} />
-      </p>
-      <ReactionButtons post={post} />
-    </article>
-  ));
+  const posts = useSelector(selectAllPosts)
+
+  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+
+  const renderedPosts = orderedPosts.map(post => (
+      <article key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.content.substring(0, 100)}</p>
+          <p className="postCredit">
+              <PostAuthor userId={post.userId} />
+              <TimeAgo timestamp={post.date} />
+          </p>
+          <ReactionButtons post={post} />
+      </article>
+  ))
+
   return (
-    <section>
-      <h2>Posts</h2>
-      {renderedPosts}
-    </section>
-  );
-};
-export default PostsList;
+      <section>
+          <h2>Posts</h2>
+          {renderedPosts}
+      </section>
+  )
+}
+export default PostsList
