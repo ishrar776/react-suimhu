1f2f0bcb230581532dfbf963bfb5f824c9f4d6f7
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-08-29 12:17:53 +0530

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import DemoCounter from './component/DemoCounter';
+import AddPostForm from './component/AddPostForm';
 import Child from './Child';
 function App() {
   return (
     <main className="App">
+      <AddPostForm />
       <DemoCounter />
       <Child />
     </main>
--- src/app/store.js
+++ src/app/store.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { configureStore } from '@reduxjs/toolkit';
 import counterSlice from '../reducer/CounterSlice';
 import postsSlice from '../reducer/postSlice';
+import usersSlice from '../reducer/usersSlice';
 export const store = configureStore({
   reducer: {
     counter1: counterSlice,
     post: postsSlice,
+    user: usersSlice,
   },
 });
