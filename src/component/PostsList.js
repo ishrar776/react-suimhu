638f45fe93e007057fcb6250e2c599f9b2f6c9ce
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-08-29 15:21:30 +0530

    4 files modified

--- src/component/PostsList.js
+++ src/component/PostsList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectAllPosts } from '../reducer/postSlice';
 import PostAuthor from '../component/PostAuthor';
--- src/component/TimeAgo.js
+++ src/component/TimeAgo.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { parseISO, formatDistanceToNow } from 'date-fns';
 const TimeAgo = ({ timestamp }) => {
   let timeAgo = '';
