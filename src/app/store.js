bbdfbd60189e4eaab1b05ba31049f5d046d2e9e6
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-08-29 11:41:39 +0530

    1 file modified

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { configureStore } from '@reduxjs/toolkit';
 import counterSlice from '../reducer/CounterSlice';
+import postsSlice from '../reducer/postSlice';
 export const store = configureStore({
   reducer: {
     counter1: counterSlice,
+    post: postsSlice,
   },
 });
